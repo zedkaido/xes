d4650783897aa97fdc89b98004991bcb5e1a3738
Zed Kaido <z@zedkaido.com>
2025-03-19 13:15:19 +0000

    v0.0.1

---
+++ README.md
@@ -0,0 +1,13 @@
+# `!XES` aka `!X` Engine Selector
+
+> Navigate the web with **!bangs** you control.
+
+Add the following url as a **`custom`**/**`default`** search engine to your browser.
+
+```
+https://xes.zedkaido.com?q=%s
+```
+
+After the first request, the JS is cached and redirects are done locally.
+
+Supported bangs: [src/bangs.js](https://github.com/zedkaido/xes/blob/master/src/bangs.js)
---
+++ src/main.js
@@ -0,0 +1,60 @@
+import { bangs } from "./bangs.js";
+
+function renderHelpPage() {
+  const url = document.URL;
+
+  const app = document.querySelector("#xes");
+  app.innerHTML = `
+    <h1><span class="hi">!XES</span> aka<br>!X Engine Selector</h1> 
+    <p><b>█</b> Navigate the web with <b class="hi">!bangs</b> you control.<p>
+    <p>Add the following url as a <b class="hi">custom</b>/<b class="hi">default</b> search engine to your browser.</p>
+    <input size="84" type="text" class="url-input" value="${url}?q=%s" readonly />
+    <p>After the first request, the JS is cached and redirects are done locally.</p>
+    <h2>Supported <span class="hi">!bangs</span></h2>
+    <p>To <span class="hi">add</span>/<span class="hi">edit</span> the bangs: <a href="https://github.com/zedkaido/xes"><b>fork</b>/<b>clone</b></a> the project to edit <a href="https://github.com/zedkaido/xes/blob/master/src/bangs.js"><b>src/bangs.js</b></a>.<br>Or, submit a <b><a href="https://github.com/zedkaido/xes/pulls">PR</a></b>/<b><a href="https://github.com/zedkaido/xes/issues/new">Issue</a></b>.</p>
+    <ul id="bangs"></ul>
+  `;
+
+  const list = document.querySelector("#bangs");
+  Object.keys(bangs).forEach(bangKey => {
+    const listItem = document.createElement("li");
+    const bang = bangs[bangKey];
+    listItem.innerHTML = `<b>!${bangKey}</b> - ${bang.name} <span class="url">:<span> <a class="url" href=${bang.url}>${bang.url}</a>`;
+    list.appendChild(listItem);
+  });
+}
+
+const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
+const defaultBang = bangs[LS_DEFAULT_BANG];
+
+function getRedirectUrl() {
+  const url = new URL(window.location.href);
+  let query = url.searchParams.get("q")?.trim() ?? "";
+  if (!query) {
+    renderHelpPage();
+    return null;
+  }
+
+  const bangMatch = query.match(/!(\S+)/i);
+  const bangCandidate = bangMatch?.[1]?.toLowerCase();
+
+  let bang = bangs[bangCandidate]
+  if (bang) query = query.replace(/!\S+\s*/i, "").trim();
+  else bang = defaultBang; 
+
+  const searchUrl = bang.url.replace(
+    "{{{s}}}",
+    encodeURIComponent(query)
+  );
+  if (!searchUrl) return null;
+
+  return searchUrl;
+}
+
+function redirect() {
+  const searchUrl = getRedirectUrl();
+  if (!searchUrl) return;
+  window.location.replace(searchUrl);
+}
+
+redirect();
